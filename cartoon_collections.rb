f78e4f43aab4b9c4fd4ed96e4d5b7ecbfb424c5b
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-06-23 13:48:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,8 @@ def summon_captain_planet(array)
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)
+  calls.any? { |e| e.length > 4 }
   # Your code here
 end
 
