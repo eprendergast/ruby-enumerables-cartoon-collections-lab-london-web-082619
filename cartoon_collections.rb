5e343053c4011d3be48c3347ae2a2707ac7d70c8
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-06-23 13:54:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,6 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-
   array.find do |element|
     element == cheese_types[0] ||
     element == cheese_types[1] ||
